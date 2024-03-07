a537e5963082382f8deae00f665cc8f0975be5cd
Yari Anrango <y.j.anrango@student.vu.nl>
2024-03-07 17:32:59 +0100

    add lstm baseline for univariate sequence

---
+++ model.py
@@ -0,0 +1,89 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from preprocessing import TrainValTest, PrematureDataset
+import pandas as pd
+
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+class LSTM(nn.Module):
+
+    def __init__(self, input_dim, hidden_dim, target_size, layer_dim):
+        super(LSTM, self).__init__()
+        self.layer_dim = layer_dim
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.target_size = target_size
+        
+        self.lstm = nn.LSTM(self.input_dim, self.hidden_dim, num_layers=self.layer_dim, batch_first=True, bidirectional=True, dropout=0.4757946679534617)
+        self.lin1 = nn.Linear(2*self.hidden_dim, self.target_size)
+
+    def forward(self, x):
+
+        lstm_out, (h, c) = self.lstm(x.float())
+        
+        last_layer_hidden_state = h.view(self.layer_dim, 2, x.size(0), self.hidden_dim)[-1]
+        
+        h_1, h_2 = last_layer_hidden_state[0], last_layer_hidden_state[1]
+        final_hidden_state = torch.cat((h_1, h_2), 1)
+
+        logits = self.lin1(final_hidden_state)
+
+        return logits
+
+df = pd.read_csv("./data/total_df.csv", index_col = 0)
+
+dataset = PrematureDataset("./data/total_df.csv")
+seed = 1
+batch_n = 5
+dataset_split = TrainValTest(dataset, 0.3, batch_n)
+train, val, test = dataset_split.train, dataset_split.val, dataset_split.test
+
+input_dim = 1
+hidden_dim = 6
+target_size = 1
+layer_dim = 3
+
+model = LSTM(input_dim, hidden_dim, target_size, layer_dim)
+
+loss_function = nn.BCEWithLogitsLoss()
+optimizer = optim.Adam(model.parameters(), lr=4.653012126454496*10**-5)
+
+train_loss = []
+val_loss_list = []
+for epoch in tqdm(range(100)):
+    model.train()
+    total_loss = 0.0
+    
+    val_loss = 0
+    for sequence, label in train:
+        model.zero_grad()
+
+        output = model(sequence.unsqueeze(-1))
+        label_correct = label.unsqueeze(-1).float()
+        loss = loss_function(output, label_correct)
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item()
+    avg_loss = total_loss/len(train)
+    train_loss.append(avg_loss)
+    
+    model.eval()
+    for sequence, label in val:
+        output = model(sequence.unsqueeze(-1))
+        label_correct = label.unsqueeze(-1).float()
+        vloss = loss_function(output, label_correct)
+        
+        val_loss += vloss.item()
+    avg_val_loss = val_loss/len(val)
+    val_loss_list.append(avg_val_loss)
+
+    
+
+plt.plot(train_loss, label = 'train loss')
+plt.plot(val_loss_list, label = 'val loss')
+plt.legend()
+plt.show()
+
