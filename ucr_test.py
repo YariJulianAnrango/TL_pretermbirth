5927e5c709cbc62b0f6215692e1943284b666366
Yari Anrango <y.j.anrango@student.vu.nl>
2024-03-22 17:17:58 +0100

    add visualization

--- ucr_test.py
+++ ucr_test.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
+import torch.optim.lr_scheduler as lr_scheduler
 
 import pandas as pd
 from sklearn import metrics
@@ -19,21 +20,21 @@ def get_ucr_data(data_dir, test_size):
     target_size = dataset.target_size
     
     train, val, test = train_val_test_split(dataset, test_size)
-
-    train_loader = DataLoader(train, batch_size = 13, shuffle=True)
-    val_loader = DataLoader(val ,batch_size = 13, shuffle=True)
-    test_loader = DataLoader(test, batch_size = 13, shuffle = True)
+    
+    train_loader = DataLoader(train, batch_size = 10, shuffle=True)
+    val_loader = DataLoader(val ,batch_size = 10, shuffle=True)
+    test_loader = DataLoader(test, batch_size = 10, shuffle = True)
     
     return train_loader, val_loader, test_loader, target_size
 
-def get_preterm_data(data_dir, test_size):
+def get_preterm_data(test_size):
     dataset = PrematureDataset("./data/total_df.csv", 1)
 
-    train, val, test = train_val_test_split(dataset, 0.3)
+    train, val, test = train_val_test_split(dataset, test_size)
     
-    train_loader = DataLoader(train, 13, shuffle=True)
-    val_loader = DataLoader(val ,13, shuffle=True)
-    test_loader = DataLoader(test ,13, shuffle=True)
+    train_loader = DataLoader(train, 4, shuffle=True)
+    val_loader = DataLoader(val ,4, shuffle=True)
+    test_loader = DataLoader(test ,4, shuffle=True)
     
     return train_loader, val_loader, test_loader
 
@@ -42,25 +43,25 @@ def get_parameters():
     pass
 
 def train_wo_transfer_learning(test_size):
-    train, val, test = get_preterm_data("./data/total_df.csv", test_size)
+    train, val, test = get_preterm_data(test_size)
 
     input_dim = 1
     hidden_dim = 15
     target_size = 1
-    layer_dim = 5
+    layer_dim = 2
 
     model = LSTM(input_dim, hidden_dim, target_size, layer_dim)
     
     loss_function = nn.BCEWithLogitsLoss()
     device = "cpu"
     
-    optimizer = optim.SGD(model.parameters(), lr=0.00034442093623802093)
+    optimizer = optim.SGD(model.parameters(), lr=0.001)
 
     train_loss = []
     val_loss_list = []
     model.to(device)
 
-    for epoch in tqdm(range(1000)):
+    for epoch in tqdm(range(500)):
         model.train()
         total_loss = 0.0
     
@@ -100,20 +101,21 @@ def train_wo_transfer_learning(test_size):
 def pretrain(train, val, target_size):
     input_dim = 1
     hidden_dim = 15
-    layer_dim = 5
+    layer_dim = 2
 
     model = LSTM(input_dim, hidden_dim, target_size, layer_dim)
 
     loss_function = nn.CrossEntropyLoss()
     device = "cpu"
     
-    optimizer = optim.SGD(model.parameters(), lr=0.00034442093623802093)
-
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    # scheduler = lr_scheduler.LinearLR(optimizer, start_factor=1.0, end_factor=0.001, total_iters=3000)
+    
     train_loss = []
     val_loss_list = []
     model.to(device)
 
-    for epoch in tqdm(range(1000)):
+    for epoch in tqdm(range(600)):
         model.train()
         total_loss = 0.0
     
@@ -126,7 +128,7 @@ def pretrain(train, val, target_size):
                 
             output = model(sequence_shaped)
             label_correct = label.to(device)
-            
+
             loss = loss_function(output, label_correct)
             loss.backward()
             optimizer.step()
@@ -135,6 +137,8 @@ def pretrain(train, val, target_size):
         avg_loss = total_loss/len(train)
         train_loss.append(avg_loss)
     
+        # scheduler.step()
+        
         model.eval()
         with torch.no_grad():
             for sequence, label in val:
@@ -161,13 +165,13 @@ def finetune(train, val, model):
     loss_function = nn.BCEWithLogitsLoss()
     device = "cpu"
     
-    optimizer = optim.SGD(model.parameters(), lr=0.00034442093623802093)
+    optimizer = optim.SGD(model.parameters(), lr=0.001)
 
     train_loss = []
     val_loss_list = []
     model.to(device)
 
-    for epoch in tqdm(range(1000)):
+    for epoch in tqdm(range(200)):
         model.train()
         total_loss = 0.0
     
@@ -221,9 +225,11 @@ def evaluate_model(train_loss, val_loss, val, model):
         sequence_shaped = sequence.float().to("cpu")
     
         logits_output = model(sequence_shaped)
-        print(logits_output)
+
         pred = sig(logits_output).round().int()
-        
+        print(logits_output, pred)
+        print(label)
+        print()
         for p in pred:
             preds.append(p.item())
         for l in label:
@@ -261,11 +267,12 @@ def evaluate_multiclass(train_loss, val_loss, val, model):
         sequence_shaped = sequence.unsqueeze(-1).float().to("cpu")
     
         logits_output = model(sequence_shaped)
-        print(logits_output)
+        print("logits \n",logits_output)
         probs = softmax(logits_output)
-        print(probs)
+        print("probs \n",probs)
         pred = torch.argmax(probs, axis = 1)
-        print(pred)
+        print("pred \n",pred)
+        print("label \n",label)
         print()
         for p in pred:
             preds.append(p.item())
@@ -277,29 +284,85 @@ def evaluate_multiclass(train_loss, val_loss, val, model):
 
     print(f"accuracy: {accuracy}, f1_score {f1_score}")
     
-
 def perform_transfer_learning(source_data_dir, test_size):
     train, val, test, target_size = get_ucr_data(source_data_dir, test_size)
 
     train_loss, val_loss, model = pretrain(train, val, target_size)
     
-    preterm_train, preterm_val, preterm_test = get_preterm_data("./data/total_df.csv", test_size)
+    preterm_train, preterm_val, preterm_test = get_preterm_data(test_size)
 
     fine_train_loss, fine_val_loss, finetuned_model = finetune(preterm_train, preterm_val, model)
     
     return fine_train_loss, fine_val_loss, preterm_val, preterm_test, finetuned_model
 
-# print("Performing training without transfer learning")
-# train_loss, val_loss_list, val, model = train_wo_transfer_learning(0.3)
-# evaluate_model(train_loss, val_loss_list, val, model)
+def visualize_seq(data):
+    for sequence, label in train: 
+        print(sequence[0])
+        print(label[0])
+    
+def overfit(train):
+    input_dim = 1
+    hidden_dim = 15
+    target_size = 1
+    layer_dim = 1
 
-# print()
-# print("Performing training with transfer learning")
-# train_loss, val_loss, val, test, finetuned_model = perform_transfer_learning("/Users/yarianrango/Documents/School/Master-AI-VU/Thesis/data/UCRArchive_2018/ECG5000/ECG5000_TRAIN.tsv", 0.3)
-# evaluate_model(train_loss, val_loss, val, finetuned_model)
+    model = LSTM(input_dim, hidden_dim, target_size, layer_dim)
+    
+    loss_function = nn.BCEWithLogitsLoss()
+    device = "cpu"
+    
+    optimizer = optim.Adam(model.parameters(), lr=0.1)
+    scheduler = lr_scheduler.LinearLR(optimizer, start_factor=1.0, end_factor=0.001, total_iters=3000)
 
-train, val, test, target_size = get_ucr_data("/Users/yarianrango/Documents/School/Master-AI-VU/Thesis/data/UCRArchive_2018/ECG5000/ECG5000_TRAIN.tsv", 0.3)
+    train_loss = []
+    val_loss_list = []
+    model.to(device)
+    sequence, label = next(iter(train))
+    sig = nn.Sigmoid()
+    for epoch in tqdm(range(10000)):
+        model.train()
+        total_loss = 0.0
+    
+        val_loss = 0
 
-train_loss, val_loss, model = pretrain(train, val, target_size)
+        model.zero_grad()
+            
+            #TODO: Fix input shape independent of data set
+        sequence_shaped = sequence.float().to(device)
+        output = model(sequence_shaped)
+            
+        label_correct = label.unsqueeze(-1).float().to(device)
+        pred = sig(output).round().int()
+        print(output, pred)
+        print(label_correct)
+        loss = loss_function(output, label_correct)
+        loss.backward()
+        optimizer.step()
 
-evaluate_multiclass(train_loss, val_loss, val, model)
+        total_loss += loss.item()
+        train_loss.append(total_loss)
+        
+        before_lr = optimizer.param_groups[0]["lr"]
+        scheduler.step()
+        after_lr = optimizer.param_groups[0]["lr"]
+        print("Epoch %d: Adam lr %.4f -> %.4f" % (epoch, before_lr, after_lr))
+    return train_loss
+print("Performing training without transfer learning")
+train_loss, val_loss_list, val, model = train_wo_transfer_learning(0.3)
+evaluate_model(train_loss, val_loss_list, val, model)
+
+
+print()
+print("Performing training with transfer learning")
+train_loss, val_loss, val, test, finetuned_model = perform_transfer_learning("/Users/yarianrango/Documents/School/Master-AI-VU/Thesis/data/UCRArchive_2018/ECG5000/ECG5000_TRAIN.tsv", 0.3)
+evaluate_model(train_loss, val_loss, val, finetuned_model)
+
+# train, val, test, target_size = get_ucr_data("/Users/yarianrango/Documents/School/Master-AI-VU/Thesis/data/UCRArchive_2018/ECG5000/ECG5000_TRAIN.tsv", 0.3)
+
+# # visualize_seq(train)
+# train_loss, val_loss, model = pretrain(train, val, target_size)
+# # train, val, test = get_preterm_data("./data/total_df.csv", 0.3)
+
+# evaluate_multiclass(train_loss, val_loss, val, model)
+
+# overfit(train)
