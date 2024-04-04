bc9b8d3fe9697984340f0af1a9bc1d2d718bb89b
Yari Anrango <y.j.anrango@student.vu.nl>
2024-04-04 11:57:01 +0200

    fix some small code

--- training_functions.py
+++ training_functions.py
@@ -10,7 +10,7 @@ from sklearn import metrics
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
-from model import LSTM
+from model import LSTM, Identity
 from preprocessing import UCRDataset, PrematureDataset, train_val_split
 
 
@@ -20,8 +20,8 @@ def get_ucr_data(data_dir, test_size, params):
     
     train, val = train_val_split(dataset, test_size)
     
-    train_loader = DataLoader(train, batch_size = params["batch_size"], shuffle=True)
-    val_loader = DataLoader(val ,batch_size = params["batch_size"], shuffle=True)
+    train_loader = DataLoader(train, batch_size = int(params["batch_size"]), shuffle=True)
+    val_loader = DataLoader(val ,batch_size = int(params["batch_size"]), shuffle=True)
     
     return train_loader, val_loader, target_size
 
@@ -30,6 +30,7 @@ def get_preterm_data(test_size, batch_size):
 
     train, val = train_val_split(dataset, test_size)
     
+    print(train.dataset)
     train_loader = DataLoader(train, batch_size, shuffle=True)
     val_loader = DataLoader(val ,batch_size, shuffle=True)
     
@@ -120,7 +121,7 @@ def pretrain(train, val, target_size, params):
     val_loss_list = []
     model.to(device)
 
-    for epoch in tqdm(range(int(500))):
+    for epoch in tqdm(range(int(params["epochs"]))):
         model.train()
         total_loss = 0.0
     
@@ -130,7 +131,7 @@ def pretrain(train, val, target_size, params):
             
             #TODO: Fix input shape independent of data set
             sequence_shaped = sequence.unsqueeze(-1).float().to(device)
-                
+
             output = model(sequence_shaped)
             label_correct = label.to(device)
 
@@ -184,10 +185,12 @@ def finetune(train, val, model, params):
     
         val_loss = 0
         for sequence, label in train: 
+
             model.zero_grad()
             
             #TODO: Fix input shape independent of data set
-            sequence_shaped = sequence.float().to(device)
+            sequence_shaped = sequence.unsqueeze(-1).float().to(device)
+
             output = model(sequence_shaped)
             
             label_correct = label.unsqueeze(-1).float().to(device)
@@ -205,7 +208,7 @@ def finetune(train, val, model, params):
         model.eval()
         with torch.no_grad():
             for sequence, label in val:
-                sequence_shaped = sequence.float().to(device)
+                sequence_shaped = sequence.unsqueeze(-1).float().to(device)
                 output = model(sequence_shaped)
                 
                 label_correct = label.unsqueeze(-1).float().to(device)
@@ -216,91 +219,7 @@ def finetune(train, val, model, params):
             val_loss_list.append(avg_val_loss)
         
     return train_loss, val_loss_list,  model
-    
-def evaluate_model(train_loss, val_loss, val, model, plot = False):
-    if plot:
-        plt.plot(train_loss, label = 'train loss')
-        plt.plot(val_loss, label = 'val loss')
-        plt.title("Train loss and val loss per epoch")
-        plt.legend()
-        plt.savefig("./figures/pca_results.png")
-        plt.show()
 
-    model.eval()
-    preds = []
-    labels = []
-
-    sig = nn.Sigmoid()
-    for sequence, label in val:
-        sequence_shaped = sequence.float().to("cpu")
-    
-        logits_output = model(sequence_shaped)
-
-        pred = sig(logits_output).round().int()
-        print("pred",pred)
-        print("label",label)
-        for p in pred:
-            preds.append(p.item())
-        for l in label:
-            labels.append(l.item())
-        
-    accuracy = metrics.accuracy_score(labels, preds)
-    f1_score = metrics.f1_score(labels, preds)
-
-    fpr, tpr, thresholds = metrics.roc_curve(labels, preds, pos_label=1)
-    auc = metrics.auc(fpr, tpr)
-
-    if plot:
-        plt.plot(fpr, tpr)
-        plt.title(f"ROC plot with AUC {auc}, accuracy {accuracy}, and f1 score {f1_score}")
-        plt.xlabel("False positive rate")
-        plt.ylabel("True positive rate")
-        plt.savefig("./figures/pca_roc.png")
-        plt.show()
-        print("f1_score", f1_score)
-        print("accuracy", accuracy)
-        print("auc", auc)
-    
-    return auc
-    
-def evaluate_multiclass(train_loss, val_loss, val, model, plot = False):
-    if plot:
-        plt.plot(train_loss, label = 'train loss')
-        plt.plot(val_loss, label = 'val loss')
-        plt.title("Train loss and val loss per epoch")
-        plt.legend()
-        plt.savefig("./figures/pca_results.png")
-        plt.show()
-
-    model.eval()
-    preds = []
-    labels = []
-
-    softmax = nn.Softmax()
-    for sequence, label in val:
-        sequence_shaped = sequence.unsqueeze(-1).float().to("cpu")
-    
-        logits_output = model(sequence_shaped)
-        # print("logits \n",logits_output)
-        probs = softmax(logits_output)
-        # print("probs \n",probs)
-        pred = torch.argmax(probs, axis = 1)
-        # print("pred \n",pred)
-        # print("label \n",label)
-        # print()
-        for p in pred:
-            preds.append(p.item())
-        for l in label:
-            labels.append(l.item())
-        
-    accuracy = metrics.accuracy_score(labels, preds)
-    f1_score = metrics.f1_score(labels, preds, average="weighted")
-
-    if plot:
-        print(f"accuracy: {accuracy}, f1_score {f1_score}")
-        
-    return f1_score
-    
 def perform_transfer_learning(source_data_dir, test_size, params):
     train, val, target_size = get_ucr_data(source_data_dir, test_size, params)
     
@@ -312,11 +231,6 @@ def perform_transfer_learning(source_data_dir, test_size, params):
     
     return fine_train_loss, fine_val_loss, preterm_val, finetuned_model
 
-def visualize_seq(data):
-    for sequence, label in data: 
-        print(sequence[0])
-        print(label[0])
-    
 def overfit(params, train):
     model = LSTM(params)
     
@@ -365,37 +279,3 @@ def overfit(params, train):
         print("Epoch %d: Adam lr %.4f -> %.4f" % (epoch, before_lr, after_lr))
     return train_loss, val_loss_list, model
 
-# print("Performing training without transfer learning")
-# train_loss, val_loss_list, val, model = train_wo_transfer_learning(0.3)
-# evaluate_model(train_loss, val_loss_list, val, model)
-
-
-# print()
-# print("Performing training with transfer learning")
-# train_loss, val_loss, val, test, finetuned_model = perform_transfer_learning("/Users/yarianrango/Documents/School/Master-AI-VU/Thesis/data/UCRArchive_2018/ECG5000/ECG5000_TRAIN.tsv", 0.3)
-# evaluate_model(train_loss, val_loss, val, finetuned_model, plot = True)
-
-# train, val, test, target_size = get_ucr_data("/Users/yarianrango/Documents/School/Master-AI-VU/Thesis/data/UCRArchive_2018/ECG5000/ECG5000_TRAIN.tsv", 0.3)
-
-# # visualize_seq(train)
-# train_loss, val_loss, model = pretrain(train, val, target_size)
-# train, val, test = get_preterm_data(0.3, 2)
-
-# evaluate_multiclass(train_loss, val_loss, val, model)
-
-# overfit(train)
-
-
-# params = {"batch_size": 2,
-#               "learning_rate": 0.1,
-#               "optimizer": "Adam",
-#               "layer_dim": 1,
-#               "hidden_dim": 15,
-#               "dropout": 0.3,
-#               "epochs": 200}
-
-# train_loss, val_loss_list, val, model = train_wo_transfer_learning(params, 0.3)
-# # # train_loss, val_loss_list, model = overfit(params, train)
-# evaluate_model(train_loss, val_loss_list, val, model, plot = True)
-
-# get_parameters("./hyperparameter_testing/parameter_testing_baseline_26:03:2024_13:19:38.txt")
