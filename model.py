31de237d7e9c39917130c70888a31f1b6124be90
Yari Anrango <y.j.anrango@student.vu.nl>
2024-04-04 11:55:13 +0200

    add multichannel model

--- model.py
+++ model.py
@@ -21,7 +21,6 @@ class LSTM(nn.Module):
         
         h_1, h_2 = last_layer_hidden_state[0], last_layer_hidden_state[1]
         final_hidden_state = torch.cat((h_1, h_2), 1)
-
         logits = self.lin1(final_hidden_state)
 
         return logits
@@ -31,6 +30,48 @@ class LSTM(nn.Module):
         c0 = torch.zeros(2*self.layer_dim, x.size(0), self.hidden_dim)
         return h0, c0
 
+class Identity(nn.Module):
+    def __init__(self):
+        super(Identity, self).__init__()
+        
+    def forward(self, x):
+        return x
+    
+class FCN(nn.Module):
+    def __init__(self, parameters):
+        super(FCN, self).__init__()
+        self.lin1 = nn.Linear(60, 256)
+        self.lin2 = nn.Linear(256, 24)
+        self.lin3 = nn.Linear(24, 1)
+    
+    def forward(self, x):
+        x1 = self.lin1(x)
+        x2 = self.lin2(x1)
+        y = self.lin3(x2)
+        
+        return y
+    
+class multichannelLSTM(nn.Module):
+    
+    def __init__(self, LSTM, parameters):
+        super(multichannelLSTM, self).__init__()
+        self.LSTM = LSTM
+        self.FCN = FCN(parameters)
+        self.flatten = nn.Flatten()
+        self.layer_dim = int(parameters["layer_dim"])
+        self.hidden_dim = int(parameters["layer_dim"])
+        
+    def forward(self, x0, x1, x2):
+        x0_out = self.LSTM(x0).unsqueeze(2)
+        x1_out = self.LSTM(x1).unsqueeze(2)
+        x2_out = self.LSTM(x2).unsqueeze(2)
+        
+        x_cat = torch.cat((x0_out, x1_out, x2_out), 2)
+        
+        x_flat = self.flatten(x_cat)
 
+        y = self.FCN(x_flat.float())
+        return y
+        
 
 
